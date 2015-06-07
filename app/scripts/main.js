325ae5a176e552f7791e7372a6e3440113238505
Rod Wilhelmy <rwilhelmy@gmail.com>
2015-06-07 16:04:13 -0400

    Load a NOAA kml and display it on the map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,8 +4,6 @@
 (function (window, document, L, undefined) {
 	'use strict';
 
-	L.Icon.Default.imagePath = 'images/';
-
 	/* create leaflet map */
 	var map = L.map('map', {
 		center: [23, -102],
@@ -19,7 +17,45 @@
 		attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
 	}).addTo(map);
 
+  var NoaaIcon = L.Icon.extend({
+    options: {
+      iconUrl: 'data/ep022015_best_track/ts_nhemi.png'
+    }
+  });
 
-	L.marker([52.5, 13.4]).addTo(map);
+  var runLayer = omnivore.kml('data/ep022015_best_track/ep022015.kml')
+    .on('ready', function() {
+      map.fitBounds(runLayer.getBounds());
+
+      runLayer.eachLayer(function(layer) {
+        var layerProperties = layer.toGeoJSON().properties;
+        var categoryName = layerProperties.styleUrl.slice(1);
+        var iconFilePrefixes = {
+          'cat1'  : 'cat1',
+          'cat2'  : 'cat2',
+          'cat3'  : 'cat3',
+          'cat4'  : 'cat4',
+          'cat5'  : 'cat5',
+          'pt'    : 'ex',
+          'db'    : 'ex',
+          'lo'    : 'ex',
+          'ex'    : 'ex',
+          'tc'    : 'ex',
+          'wv'    : 'ex',
+          'sd'    : 'ex',
+          'ss'    : 'ts',
+          'ts'    : 'ts',
+          'td'    : 'td',
+          'st'    : 'typhoon',
+          'ty'    : 'typhoon'
+        };
+        var layerIcon = new NoaaIcon({
+          iconUrl: 'data/ep022015_best_track/' + iconFilePrefixes[categoryName] + '_nhemi.png'
+        });
+        layer.setIcon(layerIcon);
+      });
+
+    })
+    .addTo(map);
 
 }(window, document, L));
